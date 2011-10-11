331eba74294900449bca60dabef56143dad7bd08
a-yakimenko <a-yakimenko@gmail.com>
2011-10-11 18:16:40 +0400

    OData first commit

---
+++ src/Nvx.Orchard.OData/Controllers/MainController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Orchard.ContentManagement;
+using Orchard.Themes;
+using Orchard.Webservice.Models;
+
+namespace Nvx.Orchard.OData.Controllers
+{
+	public class MainController : Controller
+	{
+		private readonly IContentManager _contentManager;
+
+		public MainController(IContentManager contentManager) {
+			_contentManager = contentManager;
+		}
+
+		[Themed]
+		[HttpGet]
+		public ActionResult Index(params string[] resource) 
+		{
+			TestServiceModel model = new TestServiceModel();
+			string path = Request.Url.PathAndQuery,
+				appPath = Request.ApplicationPath;
+			
+		if(!String.IsNullOrEmpty(path) && !String.IsNullOrEmpty(appPath))
+		{
+			int start = appPath.Count(),
+				count = path.Count() - appPath.Count();
+
+			// формирование относительной ссылки
+			string result = path.Substring(start, count);
+			var u = new Uri(result, UriKind.Relative);
+			// выбор параметров
+
+			// выборка всех типов Orchard
+			List<string> ApplicationTypes = _contentManager.GetContentTypeDefinitions().Select(a => a.DisplayName).ToList();
+		}
+
+		resource = Request.Url.Segments;
+			//model.XML = resource;
+
+			return View("TestService");
+		}
+
+	}
+}
---
+++ src/Nvx.Orchard.OData/Routes.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Orchard.Mvc.Routes;
+using System.Web.Routing;
+
+namespace Nvx.Orchard.OData
+{
+	public class Routes : IRouteProvider
+	{
+		public void GetRoutes(ICollection<RouteDescriptor> routes)
+		{
+			foreach (var routeDescriptor in GetRoutes())
+				routes.Add(routeDescriptor);
+		}
+
+		public IEnumerable<RouteDescriptor> GetRoutes()
+		{
+			return new RouteDescriptor[] {
+        		new RouteDescriptor {
+        			Priority = 5,
+        			Route = new Route(
+        				"OData/{res1}/{res2}/{res3}/{res4}/{res5}/{res6}/{res7}/{res8}/{res9}/{res10}",
+        				new RouteValueDictionary {
+        					{"area", "Nvx.Orchard.OData"},
+        					{"controller", "Main"},
+        					{"action", "Index"},
+							
+							{"res1", UrlParameter.Optional},
+							{"res2", UrlParameter.Optional},
+							{"res3", UrlParameter.Optional},
+							{"res4", UrlParameter.Optional},
+							{"res5", UrlParameter.Optional},
+							{"res6", UrlParameter.Optional},
+							{"res7", UrlParameter.Optional},
+							{"res8", UrlParameter.Optional},
+							{"res9", UrlParameter.Optional},
+							{"res10", UrlParameter.Optional}
+        				},
+        				new RouteValueDictionary(),
+        				new RouteValueDictionary {
+        					{"area", "Nvx.Orchard.OData"}
+        				},
+        				new MvcRouteHandler())
+        		}
+        	};
+		}
+	}
+}
